dd25d830fbe918cb31fdc59541d63c50a2c78bc9
unknown <you@example.com>
2019-12-22 00:18:23 +0100

    malopsujem

--- StudentLife/StudentLife/Controllers/MojeTrazeneVoznjeController.cs
+++ StudentLife/StudentLife/Controllers/MojeTrazeneVoznjeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace StudentLife.Controllers
 {
@@ -10,6 +11,7 @@ namespace StudentLife.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["id"] = HttpContext.Session.GetInt32("id");
             return View("mojeTrazeneVoznje");
         }
     }
