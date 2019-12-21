23ba023f6ae9334617d0e5a5b989611282e2f9b7
msinanovic2 <msinanovic2@etf.unsa.ba>
2019-12-21 13:45:12 +0100

    Model2

---
+++ StudentLife/StudentLife/Models/RegistrovaniStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace StudentLife.Models {
+    public class RegistrovaniStudent {
+
+        [Required]
+        int RegistrovaniStudentId { get; set; }
+        [Required]
+        string Ime { get; set; }
+        [Required]
+        string Prezime { get; set; }
+        [Required]
+        string Email { get; set; }
+        [Required]
+        string Password { get; set; }
+        [Required]
+        int bodovi { get; set; }
+
+
+
+    }
+}
---
+++ StudentLife/StudentLife/Models/Voznja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentLife.Models {
+    public class Voznja {
+        [ScaffoldColumn(false)]
+
+        [Required]
+        public int VoznjaID { get; set; }
+        [Required]
+        public int RegistrovaniStudentId { get; set; }
+        public RegistrovaniStudent student { get; set; }
+        [Required]
+        public DateTime polazak { get; set; }
+
+
+
+     
+
+
+    }
+}
