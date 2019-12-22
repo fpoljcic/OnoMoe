fc72abbfe5a60f55ffb72aa2cddf27d0d8322684
unknown <you@example.com>
2019-12-22 04:24:29 +0100

    krvtij

--- StudentLife/StudentLife/Controllers/NagradeController.cs
+++ StudentLife/StudentLife/Controllers/NagradeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudentLife.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace StudentLife.Controllers
 {
@@ -14,6 +16,26 @@ namespace StudentLife.Controllers
             ViewData["id"] = HttpContext.Session.GetInt32("id");
             ViewData["bodovi"] = HttpContext.Session.GetInt32("bodovi");
             return View("nagrade");
+        }
+
+        public async Task<IActionResult> skiniBodove(int brb)
+        {
+            ViewData["id"] = HttpContext.Session.GetInt32("id");
+            ViewData["bodovi"] = HttpContext.Session.GetInt32("bodovi");
+            Student s;
+            using (var db = new DatabaseContext()) {
+                s = db.Student.Where(e => e.StudentID == (int)HttpContext.Session.GetInt32("id")).FirstOrDefault();
+                if (s.Bodovi < brb) return View("nagrade");
+                s.Bodovi -= brb;
+                HttpContext.Session.SetInt32("bodovi", s.Bodovi);
+                ViewData["id"] = HttpContext.Session.GetInt32("id");
+                ViewData["bodovi"] = HttpContext.Session.GetInt32("bodovi");
+                db.Update(s);
+                await db.SaveChangesAsync();
+
+
+            }
+                return View("nagrade");
         }
 
        
